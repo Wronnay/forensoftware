e3726f2a3329de5eae14c885eaa0cc9ab0119db7
Christoph Miksche <wronnay@gmail.com>
2014-12-07 09:03:11 +0100

    RSS Changes

--- rss/posts.php
+++ rss/posts.php
@@ -34,7 +34,7 @@ include '../inc/data.php';
   
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
 ?>
-  <rss version="2.0">
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title><?php echo l291; ?> - <?php echo $site_title; ?></title>
       <link><?php echo $site_url; ?></link>
--- rss/users.php
+++ rss/users.php
@@ -31,11 +31,11 @@ include '../lang/de/1.php';
 include '../lang/en/1.php';
   }
 include '../inc/data.php';
-  header("Content-Type: application/rss+xml");
+  header("Content-Type: application/rss+xml; charset=UTF-8");
   
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
 ?>
-  <rss version="2.0">
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title><?php echo l302; ?> - <?php echo $site_title; ?></title>
       <link><?php echo $site_url; ?></link>
