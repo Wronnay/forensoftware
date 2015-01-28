807d6a819a8f191dc90fba15ab8e812f1798ae7b
Christoph Miksche <wronnay@gmail.com>
2015-01-28 19:05:12 +0100

    Update Changes

--- inc/update.php
+++ inc/update.php
@@ -1,5 +1,9 @@
 <?php
-if ($version < '0.8') {
+if ($version < '0.8.1') {
+$dbpre = $dbc->prepare("ALTER TABLE ".$PREFIX."_user ADD COLUMN act_code VARCHAR(10) NOT NULL");
+$dbpre->execute();
+$dbpre = $dbc->prepare("ALTER TABLE ".$PREFIX."_user ADD COLUMN act VARCHAR(10) NOT NULL");
+$dbpre->execute();
 $dbpre = $dbc->prepare("CREATE TABLE IF NOT EXISTS ".$PREFIX."_counter (
 id int(11) NOT NULL AUTO_INCREMENT,
 date date NOT NULL,
@@ -12,7 +16,9 @@ ip varchar(220) DEFAULT NULL,
 date datetime DEFAULT NULL
 )");
 $dbpre->execute();
-$dbpre = $dbc->prepare("INSERT INTO ".$PREFIX."_data (id, name, url, text, date, active) VALUES ('28', 'version', 'none', '0.8', now(), '0')");
+$dbpre = $dbc->prepare("INSERT INTO ".$PREFIX."_data (id, name, url, text, date, active) VALUES ('28', 'version', 'none', '0.8.1', now(), '0')");
+$dbpre->execute();
+$dbpre = $dbc->prepare("UPDATE ".$PREFIX."_data SET text = '0.8.1' WHERE id = '28'");
 $dbpre->execute();
 }
 ?>
